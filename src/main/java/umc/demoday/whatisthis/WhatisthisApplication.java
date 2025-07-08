2afdaee863b6fbcaf41cd0155d04a9966e813770
DESKTOP-N0Q4ELF\ys <ysysys1226@gmail.com>
2025-07-08 13:59:36 +0900

    [Initial] 프로젝트 생성

---
+++ src/main/java/umc/demoday/whatisthis/WhatisthisApplication.java
@@ -0,0 +1,13 @@
+package umc.demoday.whatisthis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatisthisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatisthisApplication.class, args);
+	}
+
+}
