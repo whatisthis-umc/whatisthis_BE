02a8cebca2e6b64efa54831e59b0bb56e51b12e2
DESKTOP-N0Q4ELF\ys <ysysys1226@gmail.com>
2025-07-10 22:07:24 +0900

    [Chore] 스웨거 설정 및 적용

--- src/main/java/umc/demoday/whatisthis/global/HomeController.java
+++ src/main/java/umc/demoday/whatisthis/global/HomeController.java
@@ -1,5 +1,6 @@
 package umc.demoday.whatisthis.global;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping("/")
+    @Operation(summary = "홈 화면 안내문구 -by 윤영석")
     public String home() {
         return "이게 뭐예요 스프링 서버입니다!";
     }
---
+++ src/main/java/umc/demoday/whatisthis/global/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package umc.demoday.whatisthis.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI swagger() {
+        Info info = new Info().title("이게뭐예요").description("Umc 8기 Demoday 이게뭐예요 Swagger").version("0.0.1");
+
+        String securityScheme = "JWT TOKEN";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityScheme);
+
+        Components components = new Components()
+                .addSecuritySchemes(securityScheme, new SecurityScheme()
+                        .name(securityScheme)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("Bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(info)
+                .addServersItem(new Server().url("/"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
