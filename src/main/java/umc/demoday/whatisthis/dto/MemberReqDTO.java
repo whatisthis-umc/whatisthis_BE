693bfc9448b0a01c7c59eb1b7c4ed833e017c5cd
이정준 <ljj10571@gmail.com>
2025-07-14 15:06:54 +0900

    [Feat] JoinRequestDto 추가

---
+++ src/main/java/umc/demoday/whatisthis/dto/MemberReqDTO.java
@@ -0,0 +1,15 @@
+package umc.demoday.whatisthis.dto;
+
+public class MemberReqDTO {
+
+    public record JoinRequestDto(
+        String email,
+        String emailAuthCode, // 이메일 인증 코드
+        String username, // 사용자가 입력한 아이디
+        String password,
+        String nickname,
+        Boolean serviceAgreed,
+        Boolean privacyAgreed
+    ) {}
+
+}
