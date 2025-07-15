e2dae9c7e48225dfb9a0db7b3e6e2fee10102080
이정준 <ljj10571@gmail.com>
2025-07-16 02:46:19 +0900

    [Feat] SecurityConfig 회원가입 인증 없음 추가

--- src/main/java/umc/demoday/whatisthis/global/config/SecurityConfig.java
+++ src/main/java/umc/demoday/whatisthis/global/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package umc.demoday.whatisthis.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -12,5 +14,17 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/v0/members/signup").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
     }
 }
