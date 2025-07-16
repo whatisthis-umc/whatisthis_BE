90e3830a607767c5575e28c40bf7ca54e2b2e495
이정준 <ljj10571@gmail.com>
2025-07-16 13:03:09 +0900

    [Feat] 이메인 인증 제외 컨트롤러 테스트 추가

--- src/test/java/umc/demoday/whatisthis/controller/member/MemberControllerValidationTest.java
+++ src/test/java/umc/demoday/whatisthis/controller/member/MemberControllerValidationTest.java
@@ -15,7 +15,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.profiles.active=test"
+})
 @AutoConfigureMockMvc
 @Transactional
 public class MemberControllerValidationTest {
