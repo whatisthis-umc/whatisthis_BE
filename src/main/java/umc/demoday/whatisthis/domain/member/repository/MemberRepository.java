30984e9010e23c3708117450dfd82bf18e784516
이정준 <ljj10571@gmail.com>
2025-07-17 22:56:23 +0900

    [Feat] findByMemberId 메서드 추가

--- src/main/java/umc/demoday/whatisthis/domain/member/repository/MemberRepository.java
+++ src/main/java/umc/demoday/whatisthis/domain/member/repository/MemberRepository.java
@@ -3,9 +3,13 @@ package umc.demoday.whatisthis.domain.member.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.demoday.whatisthis.domain.member.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByNickname(String nickname);
 
     boolean existsByMemberId(String memberId);
+
+    Optional<Member> findByMemberId(String memberId);
 }
