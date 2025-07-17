827191f0c2be5721e83907b4a5beeb1b1a7babc9
이정준 <ljj10571@gmail.com>
2025-07-17 23:18:18 +0900

    [Chore] @AllArgsConstructor 추가

--- src/main/java/umc/demoday/whatisthis/domain/member/dto/login/LoginResDTO.java
+++ src/main/java/umc/demoday/whatisthis/domain/member/dto/login/LoginResDTO.java
@@ -1,8 +1,10 @@
 package umc.demoday.whatisthis.domain.member.dto.login;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class LoginResDTO {
 
     private String accessToken;
