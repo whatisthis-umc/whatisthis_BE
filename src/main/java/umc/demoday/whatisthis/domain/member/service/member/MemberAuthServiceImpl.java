2994b54d211ebfeab17d1499a6a4bef29f303fbc
이정준 <ljj10571@gmail.com>
2025-07-17 23:19:04 +0900

    [Feat] refreshToken DB에 저장 기능 추가

--- src/main/java/umc/demoday/whatisthis/domain/member/service/member/MemberAuthServiceImpl.java
+++ src/main/java/umc/demoday/whatisthis/domain/member/service/member/MemberAuthServiceImpl.java
@@ -7,8 +7,11 @@ import umc.demoday.whatisthis.domain.member.Member;
 import umc.demoday.whatisthis.domain.member.dto.login.LoginReqDTO;
 import umc.demoday.whatisthis.domain.member.dto.login.LoginResDTO;
 import umc.demoday.whatisthis.domain.member.repository.MemberRepository;
+import umc.demoday.whatisthis.domain.refresh_token.RefreshToken;
+import umc.demoday.whatisthis.domain.refresh_token.repository.RefreshTokenRepository;
 import umc.demoday.whatisthis.global.apiPayload.code.GeneralErrorCode;
 import umc.demoday.whatisthis.global.apiPayload.exception.GeneralException;
+import umc.demoday.whatisthis.global.security.JwtProvider;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +20,7 @@ public class MemberAuthServiceImpl implements MemberAuthService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtProvider jwtProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     @Override
     public LoginResDTO login(LoginReqDTO request) {
@@ -29,7 +33,10 @@ public class MemberAuthServiceImpl implements MemberAuthService {
         }
 
         String accessToken = jwtProvider.createAccessToken(member.getId(), "ROLE_USER");
+        String refreshToken = jwtProvider.createRefreshToken(member.getId());
 
-        return new LoginResDTO(accessToken, null);
+        refreshTokenRepository.save(new RefreshToken(member.getId(), refreshToken));
+
+        return new LoginResDTO(accessToken, refreshToken);
     }
 }
