936e79378dd73addf8bacd1d26a946b504ef2a10
c5ln <cjstjdgh0209@gmail.com>
2025-07-17 18:02:14 +0900

    feat : PostController, Service 어노테이션 추가

--- src/main/java/umc/demoday/whatisthis/domain/post/controller/PostController.java
+++ src/main/java/umc/demoday/whatisthis/domain/post/controller/PostController.java
@@ -1,4 +1,12 @@
 package umc.demoday.whatisthis.domain.post.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/posts")
 public class PostController {
+
 }
--- src/main/java/umc/demoday/whatisthis/domain/post/service/PostServiceImpl.java
+++ src/main/java/umc/demoday/whatisthis/domain/post/service/PostServiceImpl.java
@@ -1,5 +1,12 @@
 package umc.demoday.whatisthis.domain.post.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class PostServiceImpl implements PostService {
 
 }
