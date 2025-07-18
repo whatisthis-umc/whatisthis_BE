39647203a4e7ba1f1bde64188e0a0c41c17fdb77
이정준 <ljj10571@gmail.com>
2025-07-18 15:35:25 +0900

    [Feat] AdminAuthController에 관리자 accessToken 재발급 API추가

--- src/main/java/umc/demoday/whatisthis/domain/admin/controller/AdminAuthController.java
+++ src/main/java/umc/demoday/whatisthis/domain/admin/controller/AdminAuthController.java
@@ -1,12 +1,10 @@
 package umc.demoday.whatisthis.domain.admin.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import umc.demoday.whatisthis.domain.admin.dto.AdminLoginReqDTO;
 import umc.demoday.whatisthis.domain.admin.dto.AdminLoginResDTO;
 import umc.demoday.whatisthis.domain.admin.service.AdminAuthService;
@@ -25,4 +23,13 @@ public class AdminAuthController {
     ) {
         AdminLoginResDTO response = adminAuthService.login(request);
         return CustomResponse.ok(response);
+    }
+
+    @PostMapping("/reissue")
+    @Operation(summary = "관리자 accessToken 재발급 API -by 이정준", security = {@SecurityRequirement(name = "")})
+    public CustomResponse<AdminLoginResDTO> reissue(@RequestHeader("Refresh-Token") String refreshToken) {
+        AdminLoginResDTO response = adminAuthService.reissue(refreshToken);
+        return CustomResponse.ok(response);
+    }
+
 }
