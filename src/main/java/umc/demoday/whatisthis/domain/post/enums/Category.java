bb138ad081a319aed86085ef322cc5a75e7f7a2f
nsh0919 <chelsea4570@naver.com>
2025-07-19 13:39:40 +0900

    카테고리 ENUM 내용물 기입

--- src/main/java/umc/demoday/whatisthis/domain/post/enums/Category.java
+++ src/main/java/umc/demoday/whatisthis/domain/post/enums/Category.java
@@ -1,4 +1,12 @@
 package umc.demoday.whatisthis.domain.post.enums;
 
 public enum Category {
+
+    LIFE_TIP,LIFE_ITEM, // 생활꿀팁, 생활꿀템
+
+    TIP,    // 커뮤니티-생활꿀팁
+    ITEM,   // 커뮤니티-꿀템추천
+    SHOULD_I_BUY,   // 커뮤니티-살까말까?
+    CURIOUS;    // 커뮤니티-궁금해요!
+
 }
