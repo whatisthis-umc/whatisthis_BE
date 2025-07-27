5f387745bcc3444b3d070982de41570db845e79f
이정준 <ljj10571@gmail.com>
2025-07-27 21:58:24 +0900

    [Feat] 비밀번호 재설정 이메일 인증 코드 전송 API 추가

--- src/main/java/umc/demoday/whatisthis/domain/member/controller/MemberFindController.java
+++ src/main/java/umc/demoday/whatisthis/domain/member/controller/MemberFindController.java
@@ -8,10 +8,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import umc.demoday.whatisthis.domain.member.Member;
 import umc.demoday.whatisthis.domain.member.dto.member.FindIdReqDTO;
 import umc.demoday.whatisthis.domain.member.dto.member.FindIdResDTO;
+import umc.demoday.whatisthis.domain.member.dto.member.PasswordResetReqDTO;
+import umc.demoday.whatisthis.domain.member.service.email.PasswordResetService;
 import umc.demoday.whatisthis.domain.member.service.member.MemberQueryService;
 import umc.demoday.whatisthis.global.apiPayload.CustomResponse;
+import umc.demoday.whatisthis.global.apiPayload.code.GeneralSuccessCode;
 
 @RestController
 @RequestMapping("/members")
@@ -19,6 +23,7 @@ import umc.demoday.whatisthis.global.apiPayload.CustomResponse;
 public class MemberFindController {
 
     private final MemberQueryService memberQueryService;
+    private final PasswordResetService passwordResetService;
 
     @PostMapping("/find-id")
     @Operation(summary = "아이디 찾기 API - by 이정준")
@@ -28,4 +33,12 @@ public class MemberFindController {
         return ResponseEntity.ok(CustomResponse.ok(response));
     }
 
+    @PostMapping("/reset-password/send-code")
+    @Operation(summary = "비밀번호 재설정 - 인증코드 발송 API -by 이정준")
+    public CustomResponse<Void> sendResetCode (
+            @RequestBody @Valid PasswordResetReqDTO request) {
+
+        passwordResetService.sendResetCode(request.getMemberId(), request.getFullEmail());
+        return CustomResponse.onSuccess(GeneralSuccessCode.EMAIL_AUTH_SENT, null);
+    }
 }
