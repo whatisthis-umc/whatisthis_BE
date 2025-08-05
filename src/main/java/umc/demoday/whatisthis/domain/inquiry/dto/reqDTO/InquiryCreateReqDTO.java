10e78e2a7d37d94ed715dea544c4940a848c3643
noonbbara <ysys1226@naver.com>
2025-08-05 23:45:13 +0900

    [Feat] 문의 등록 구현

--- src/main/java/umc/demoday/whatisthis/domain/inquiry/dto/reqDTO/InquiryCreateReqDTO.java
+++ src/main/java/umc/demoday/whatisthis/domain/inquiry/dto/reqDTO/InquiryCreateReqDTO.java
@@ -2,8 +2,12 @@ package umc.demoday.whatisthis.domain.inquiry.dto.reqDTO;
 
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class InquiryCreateReqDTO {
     private String title;
     private String content;
+    private Boolean isSecret;
+    // private List<String> fileUrls; // ← 첨부파일 URL 리스트
 }
