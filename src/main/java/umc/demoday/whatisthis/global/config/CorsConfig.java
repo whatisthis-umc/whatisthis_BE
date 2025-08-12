694250c6e80ff1ccf24e253daf70fa40c0ab8d9e
noonbbara <149371593+noonbbara@users.noreply.github.com>
2025-08-12 18:12:04 +0900

    [Chore] 도메인 cors 설정 (#176)

--- src/main/java/umc/demoday/whatisthis/global/config/CorsConfig.java
+++ src/main/java/umc/demoday/whatisthis/global/config/CorsConfig.java
@@ -7,27 +7,32 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CorsConfig {
     public static CorsConfigurationSource apiConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        ArrayList<String> allowedOriginPatterns = new ArrayList<>();
+        List<String> allowedOriginPatterns = new ArrayList<>();
         allowedOriginPatterns.add("http://localhost:8080");
         allowedOriginPatterns.add("http://localhost:5173");
+        allowedOriginPatterns.add("https://api.whatisthis.co.kr");
 
-        ArrayList<String> allowedHttpMethods = new ArrayList<>();
+        List<String> allowedHttpMethods = new ArrayList<>();
         allowedHttpMethods.add("GET");
         allowedHttpMethods.add("POST");
         allowedHttpMethods.add("PATCH");
         allowedHttpMethods.add("PUT");
         allowedHttpMethods.add("DELETE");
 
-        configuration.setAllowedOrigins(allowedOriginPatterns);
+        configuration.setAllowedOriginPatterns(allowedOriginPatterns);
         configuration.setAllowedMethods(allowedHttpMethods);
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedHeader("*");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // /** 경로 패턴은 모든 하위 경로까지 CORS 적용
         source.registerCorsConfiguration("/**", configuration);
 
         return source;
