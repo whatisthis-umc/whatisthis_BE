718890476904eef5862a26a3d2214900fd48c6d5
이정준 <ljj10571@gmail.com>
2025-08-12 14:05:53 +0900

    [Fix] CustomUserDetailsService 사용 방식으로 변경

--- src/main/java/umc/demoday/whatisthis/global/config/SecurityConfig.java
+++ src/main/java/umc/demoday/whatisthis/global/config/SecurityConfig.java
@@ -1,7 +1,11 @@
 package umc.demoday.whatisthis.global.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,16 +15,34 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import umc.demoday.whatisthis.global.security.JwtAuthenticationFilter;
+import umc.demoday.whatisthis.global.service.CustomUserDetailsService;
 
 import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomUserDetailsService customUserDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     // PasswordEncoder Bean 등록
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider dap = new DaoAuthenticationProvider();
+        dap.setUserDetailsService(customUserDetailsService);
+        dap.setPasswordEncoder(passwordEncoder());
+        return dap;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager(); // DaoAuthenticationProvider가 내부에 등록됨
     }
 
     @Bean
@@ -37,42 +59,43 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(
-                                "/members/signup",
-                                "/members/email-auth",
-                                "/admin/login",
-                                "/admin/reissue",
-                                "/members/login",
-                                "/members/reissue",
-                                "/members/find-id",
-                                "/members/reset-password/send-code",
-                                "/members/reset-password/verify-code",
-                                "/members/reset-password",
+                                .requestMatchers(
+                                        "/members/signup",
+                                        "/members/email-auth",
+                                        "/admin/login",
+                                        "/admin/reissue",
+                                        "/members/login",
+                                        "/members/reissue",
+                                        "/members/find-id",
+                                        "/members/reset-password/send-code",
+                                        "/members/reset-password/verify-code",
+                                        "/members/reset-password",
 
-                                "/support/notices",
-                                "/support/notices/*",
-                                "/support/inquiries",
-                                "/support/inquiries/*",
+                                        "/support/notices",
+                                        "/support/notices/*",
+                                        "/support/inquiries",
+                                        "/support/inquiries/*",
 
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**",
-                                "/swagger-resources/**",
-                                "/webjars/**",
+                                        "/swagger-ui/**",
+                                        "/v3/api-docs/**",
+                                        "/swagger-resources/**",
+                                        "/webjars/**",
 
 //                              "/posts/**",
 
-                                "/upload" // S3 테스트 용, 추후 삭제(?)
-                        ).permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/members/**").hasRole("USER")
+                                        "/upload" // S3 테스트 용, 추후 삭제(?)
+                                ).permitAll()
+                                .requestMatchers("/admin/**").hasRole("ADMIN")
+                                .requestMatchers("/members/**").hasRole("USER")
                                 .requestMatchers("/posts/**").hasAnyRole("USER", "ADMIN")
-                        .anyRequest().authenticated()
+                                .anyRequest().authenticated()
                 )
+                .authenticationProvider(daoAuthenticationProvider())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
