9faba712d189f5e0b4743636652a721ce9d3a7af
이정준 <ljj10571@gmail.com>
2025-08-12 15:21:50 +0900

    [Feat] 로그아웃 api 컨트롤러 추가

--- src/main/java/umc/demoday/whatisthis/domain/member/controller/MemberAuthController.java
+++ src/main/java/umc/demoday/whatisthis/domain/member/controller/MemberAuthController.java
@@ -4,10 +4,12 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import umc.demoday.whatisthis.domain.member.dto.login.LoginReqDTO;
 import umc.demoday.whatisthis.domain.member.dto.login.LoginResDTO;
 import umc.demoday.whatisthis.domain.member.service.member.MemberAuthService;
+import umc.demoday.whatisthis.global.CustomUserDetails;
 import umc.demoday.whatisthis.global.apiPayload.CustomResponse;
 
 @RestController
@@ -31,5 +33,12 @@ public class MemberAuthController {
     public CustomResponse<LoginResDTO> reissue(@RequestHeader("Refresh-Token") String refreshToken) {
         LoginResDTO response = memberAuthService.reissue(refreshToken);
         return CustomResponse.ok(response);
+    }
+
+    @PostMapping("/logout")
+    @Operation(summary = "일반 사용자 로그아웃 API -by 이정준")
+    public CustomResponse<Void> logout(@AuthenticationPrincipal CustomUserDetails member) {
+        memberAuthService.logout(member.getId());
+        return CustomResponse.ok(null);
     }
 }
