79b34024bc65d7b2ae4dbed2709d8d72b74cac5a
noonbbara <149371593+noonbbara@users.noreply.github.com>
2025-08-12 14:45:56 +0900

    [Refactor] customUserDetail 도입

--- src/main/java/umc/demoday/whatisthis/domain/post_scrap/controller/PostScrapController.java
+++ src/main/java/umc/demoday/whatisthis/domain/post_scrap/controller/PostScrapController.java
@@ -11,11 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import umc.demoday.whatisthis.domain.member.Member;
+import umc.demoday.whatisthis.domain.member.repository.MemberRepository;
 import umc.demoday.whatisthis.domain.post.dto.PostResponseDTO;
 import umc.demoday.whatisthis.domain.post_scrap.dto.resDTO.MyScrapPageResDTO;
 import umc.demoday.whatisthis.domain.post_scrap.service.PostScrapQueryService;
+import umc.demoday.whatisthis.global.CustomUserDetails;
 import umc.demoday.whatisthis.global.apiPayload.CustomResponse;
 import umc.demoday.whatisthis.global.apiPayload.code.GeneralSuccessCode;
+import umc.demoday.whatisthis.global.apiPayload.exception.GeneralException;
+
+import static umc.demoday.whatisthis.global.apiPayload.code.GeneralErrorCode.MEMBER_NOT_FOUND;
 
 @RestController
 @RequestMapping("/scraps")
@@ -23,13 +28,17 @@ import umc.demoday.whatisthis.global.apiPayload.code.GeneralSuccessCode;
 public class PostScrapController {
 
     private final PostScrapQueryService postScrapQueryService;
+    private final MemberRepository memberRepository;
 
     @GetMapping
     @Operation(summary = "스크랩 목록 기본 : 10개씩 -by 윤영석")
     public CustomResponse<MyScrapPageResDTO> getMyScrapList(
             @RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "10") int size,
-            @AuthenticationPrincipal Member loginUser) {
+            @AuthenticationPrincipal CustomUserDetails user) {
+
+        Member loginUser = memberRepository.findById(user.getId())
+                .orElseThrow(() -> new GeneralException(MEMBER_NOT_FOUND));
 
         Pageable pageable = PageRequest.of(Math.max(page - 1, 0), size, Sort.by(Sort.Direction.DESC, "post.createdAt"));
 
