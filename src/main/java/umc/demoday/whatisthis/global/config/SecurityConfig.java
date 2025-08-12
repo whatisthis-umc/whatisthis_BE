155b1d1f8e4fa8394d197e2888fe84f40cc1cdf9
c5ln <70241022+c5ln@users.noreply.github.com>
2025-08-12 12:46:53 +0900

    Fix : CorsConfiguration Source Allowed Method 수정

--- src/main/java/umc/demoday/whatisthis/global/config/SecurityConfig.java
+++ src/main/java/umc/demoday/whatisthis/global/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:5173")); // 프론트 로컬 주소
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
