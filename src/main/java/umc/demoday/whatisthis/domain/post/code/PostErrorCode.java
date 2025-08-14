cc6c0f8b545702887a332ec34665876c2a2fe3ae
nsh0919 <chelsea4570@naver.com>
2025-08-15 04:19:16 +0900

    [fix] 수정완료

--- src/main/java/umc/demoday/whatisthis/domain/post/code/PostErrorCode.java
+++ src/main/java/umc/demoday/whatisthis/domain/post/code/PostErrorCode.java
@@ -13,7 +13,8 @@ public enum PostErrorCode implements BaseErrorCode {
     POST_NOT_FOUND(HttpStatus.NOT_FOUND,"POST4041","존재하지 않는 게시물입니다."),
     ALREADY_LIKED_POST(HttpStatus.CONFLICT,"POST4091", "이미 좋아요 등록한 게시물입니다."),
     ALREADY_UNLIKED_POST(HttpStatus.CONFLICT,"POST4092", "이미 좋아요가 없는 게시물입니다."),
-    CANNOT_LIKE_OWN_POST(HttpStatus.FORBIDDEN, "POST4031", "자신의 게시물은 좋아요할 수 없습니다.")
+    CANNOT_LIKE_OWN_POST(HttpStatus.FORBIDDEN, "POST4031", "자신의 게시물은 좋아요할 수 없습니다."),
+    NOT_COMMUNITY(HttpStatus.BAD_REQUEST,"POST4002", "커뮤니티 카테고리가 아닙니다.")
 
     //INVALID_CATEGORY(HttpStatus.BAD_REQUEST,"")
     ;
