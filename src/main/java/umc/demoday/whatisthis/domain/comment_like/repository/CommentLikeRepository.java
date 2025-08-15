c8e13d391847520ee7f5c390b6dc953fae747fe3
nsh0919 <chelsea4570@naver.com>
2025-08-15 22:50:14 +0900

    중간 저장 커밋

--- src/main/java/umc/demoday/whatisthis/domain/comment_like/repository/CommentLikeRepository.java
+++ src/main/java/umc/demoday/whatisthis/domain/comment_like/repository/CommentLikeRepository.java
@@ -1,9 +1,14 @@
 package umc.demoday.whatisthis.domain.comment_like.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import umc.demoday.whatisthis.domain.comment.Comment;
 import umc.demoday.whatisthis.domain.comment_like.CommentLike;
 import umc.demoday.whatisthis.domain.member.Member;
+
+import java.util.List;
 
 public interface CommentLikeRepository extends JpaRepository<CommentLike, Integer> {
     Integer countByMember(Member member);
@@ -13,4 +18,8 @@ public interface CommentLikeRepository extends JpaRepository<CommentLike, Intege
     void deleteCommentLikeByCommentAndMember(Comment comment, Member member);
 
     boolean existsByMemberAndComment(Member member, Comment comment);
+
+    @Modifying
+    @Query("delete from CommentLike cl where cl.comment.id in :commentIds")
+    void deleteByCommentIds(@Param("commentIds") List<Integer> commentIds);
 }
